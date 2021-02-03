eeb124c21ad53607c2f805e68c90abd27e9499e2
Yogesh Kumar <yogeshykk5@gmail.com>
2021-02-03 13:40:44 +0530

    Second Commit

--- src/main/java/com/l2c/springdemo/config/DemoAppConfig.java
+++ src/main/java/com/l2c/springdemo/config/DemoAppConfig.java
@@ -23,7 +23,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
-
+   //
 @Configuration
 @EnableWebMvc
 @EnableTransactionManagement
